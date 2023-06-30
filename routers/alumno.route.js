ec341423586e38d60152e5632bd77db8060325c6
Sofia Munoz <sofia.munoz@valtech.com>
2023-06-30 13:05:11 -0300

    Agrego rutas para metodos de alumno

--- routers/alumno.route.js
+++ routers/alumno.route.js
@@ -4,12 +4,15 @@ const alumnoCtrl = require('./../controllers/alumno.controller');
 const express = require('express');
 const router = express.Router();
 
+router.get('/', alumnoCtrl.getAlumnos);
+router.get('/:idalumno',alumnoCtrl.getAlumno);
+router.post('/', alumnoCtrl.createAlumno);
+
 router.post('/login', alumnoCtrl.loginAlumno);
 router.put('/:idalumno/rutinas/:idrutina/asistencia', alumnoCtrl.registrarAsistencia);
 router.get('/:idalumno/rutinas', alumnoCtrl.getRutinasAsignadas);
 router.get('/:idalumno/asistencias', alumnoCtrl.getRutinasConAsistencia);
 router.put('/:idalumno', alumnoCtrl.updateAlumno);
-router.get('/:idalumno',alumnoCtrl.getAlumno);
 router.get('/:idalumno/publicaciones',alumnoCtrl.getPublicaciones);
 router.put('/:idalumno/publicaciones',alumnoCtrl.createPublicacion);
 
