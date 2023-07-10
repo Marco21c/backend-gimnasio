6f0ac937c247e452ce9c8b61c086e970556e8616
DESKTOP-K4V3L7P\MI NOTEBOOK <marcoscondori@gmail.com>
2023-07-10 17:48:06 -0300

    Cambios de publicaciones

--- models/publicacion.js
+++ models/publicacion.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const PublicacionSchema = new Schema({      
-    fecha : {type:String, required: false},
-    descripcion : {type:String,required:true}
+    fecha : {type:String, required: true},
+    descripcion : {type:String,required:true},
+    imagen: {type:String,required:true}
 })
 
 module.exports = mongoose.models.Publicacion || mongoose.model('Publicacion', PublicacionSchema);
